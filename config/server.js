4c6dae75c8171c2676e180aa85a60ea330821981
unknown <stefaleon@gmail.com>
2021-05-10 00:09:06 +0300

    fsm starter

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2da7296a892e45bb36d32672b89c3695'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b8a92c72-0007-4f30-b1c7-ed3d74368cba'
+};
